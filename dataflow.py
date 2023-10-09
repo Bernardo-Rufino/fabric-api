dec5903c782100f1d6866a12536a3a9c6351acf2
Bernardo Rufino <b.rufino@live.com>
2023-10-08 22:05:30 -0300

    Updated create_dataflow functionality.

--- dataflow.py
+++ dataflow.py
@@ -133,37 +133,22 @@ class Dataflow:
         """
 
         # If both, user and workspace if are provided...
-        if (user_principal_name != '') & (workspace_id != ''):
+        if (dataflow_content != '') & (workspace_id != ''):
 
-            request_url = self.main_url + f'/groups/{workspace_id}/users'
+            request_url = self.main_url + f'/groups/{workspace_id}/imports?datasetDisplayName=model.json&nameConflit=Ignore'
 
-            headers = {'Authorization': f'Bearer {self.token}'}
-
-            # Add user to workspace with the specified access right.
-            # https://learn.microsoft.com/en-us/rest/api/power-bi/groups/add-group-user#groupuseraccessright
-
-            # If service principal account
-            if user_type == 'SP':
-                data = {
-                    "identifier": user_principal_name,
-                    "groupUserAccessRight": access_right
-                }
-
-            else:
-
-                data = {
-                    "emailAddress": user_principal_name,
-                    "groupUserAccessRight": access_right
-                }
-
+            body = {
+                'value': json.dumps(dataflow_content, ensure_ascii=True)
+            }
+            
             # Make the request
-            r = requests.post(url=request_url, headers=headers, json=data)
+            r = requests.post(url=request_url, headers=self.headers, files=body)
 
             # Get HTTP status and content
             status = r.status_code
 
             # If success...
-            if status == 200:
+            if status in (200, 202):
                 return {'message': 'Success'}
             
             else:                
@@ -180,147 +165,3 @@ class Dataflow:
 
         else:
             return {'message': 'Missing parameters, please check.'}
-
-
-    def update_user(
-                self, 
-                user_principal_name: str = '', 
-                workspace_id: str = '', 
-                access_right: str = 'Member') -> Dict:
-        """
-        Update an user on a workspace.
-
-        Args:
-            user_principal_name (str, optional): user e-mail or identifier of service principal.
-            workspace_id (str, optional): workspace id to add the user to.
-            access_right (str, optional): access right type. Defaults to 'Member'.
-
-        Returns:
-            Dict: status message.
-        """
-
-        # If both, user and workspace if are provided...
-        if (user_principal_name != '') & (workspace_id != ''):
-
-            request_url = self.main_url + f'/groups/{workspace_id}/users'
-
-            headers = {'Authorization': f'Bearer {self.token}'}
-
-            # Add user to workspace with the specified access right.
-            # https://learn.microsoft.com/en-us/rest/api/power-bi/groups/update-group-user
-            data = {
-                "identifier": user_principal_name,
-                "groupUserAccessRight": access_right,
-                "principalType": "User"
-            }
-
-            # Make the request
-            r = requests.put(url=request_url, headers=headers, json=data)
-
-            # Get HTTP status and content
-            status = r.status_code
-
-            # If success...
-            if status == 200:
-                return {'message': 'Success'}
-            
-            else:
-                
-                response = json.loads(r.content)                
-                # If any error happens, return message.
-                error_message = response['error']['code']
-
-                return {'message': {'error': error_message, 'content': response}}
-
-        else:
-            return {'message': 'Missing parameters, please check.'}
-
-
-    def remove_user(self, user_principal_name: str = '', workspace_id: str = '') -> Dict:
-        """
-        Remove an user from a workspace.
-
-        Args:
-            user_principal_name (str, optional): user e-mail or identifier of service principal.
-            workspace_id (str, optional): workspace id to add the user to.
-
-        Returns:
-            Dict: status message
-        """
-
-        # If both, user and workspace if are provided...
-        if (user_principal_name != '') & (workspace_id != ''):
-
-            request_url = self.main_url + f'/groups/{workspace_id}/users/{user_principal_name}'
-
-            headers = {'Authorization': f'Bearer {self.token}'}
-
-            # Make the request
-            r = requests.delete(url=request_url, headers=headers)
-
-            # Get HTTP status and content
-            status = r.status_code
-            response = json.loads(r.content)
-
-            # If success...
-            if status == 200:
-                return {'message': 'Success'}
-
-            else:                
-                # If any error happens, return message.
-                error_message = response['error']['message']
-
-                return {'message': {'error': error_message, 'content': response}}
-
-        else:
-            return {'message': 'Missing parameters, please check.'}
-
-
-    def batch_update_user(self, user: str = '', workspaces_list: List[str] = []) -> DataFrame:
-        """
-        Batch update an user on a list of workspaces.
-
-        Args:
-            user (str): user e-mail or identifier of service principal.
-            workspaces_list (List[str]): list of workspaces to update an user.
-
-        Returns:
-            DataFrame: table with workspaces and status of the update.
-        """
-
-        responses = []
-
-        # If user and list of workspaces were informed...
-        if (user != '') & (workspaces_list != []):
-
-
-            for workspace in workspaces_list:
-                id = workspace.get('id', '')
-                name = workspace.get('name', '')
-                response = self.update_user(user_principal_name=user, workspace_id=id, access_right='Admin')
-
-                # Try to update the user
-                try:
-                    responses.append((id, name, 'Error', response['message']['content']))
-                except:
-                    responses.append((id, name, 'Success', ''))
-
-            # Create a dataframe with responses
-            df1 = pd.DataFrame(responses, columns=['id', 'name', 'status', 'error_message'])
-
-            # Serialize json from error message as a new dataframe
-            df2 = pd.json_normalize(df1['error_message'])
-
-            # Drop error message column and merge both dataframes
-            df1.drop(labels='error_message', axis='columns', inplace=True)
-            df = pd.merge(left=df1, right=df2, left_index=True, right_index=True)
-            df = df.fillna('')
-
-            # Save to an Excel file with user name
-            df.to_excel(f"./data/workspaces_{user.split('@')[0]}.xlsx", index=False)
-
-            return df
-
-        else:
-
-            return pd.DataFrame([], columns=['id', 'name', 'status', 'error_message'])
