6969624084c197e23f80ca7b76b09a43d8feef5a
Bernardo Rufino <b.rufino@live.com>
2024-08-20 18:11:57 -0300

    Added export to json functionality.

--- dataflow.py
+++ dataflow.py
@@ -211,3 +211,51 @@ class Dataflow:
 
             else:
                 return {'message': 'Missing parameters, please check.'}
+            
+
+    def export_dataflow_json(self, workspace_id: str = '', dataflow_id: str = '', dataflow_name: str = '') -> Dict:
+        """
+        Get all details from a specific dataflows in a workspace_id that the user has access to.
+
+        Args:
+            workspace_id (str, optional): workspace id to search for.
+            dataflow_id (str, optional): dataflow id to get the details.
+
+        Returns:
+            Dict: status message and content.
+        """
+        # Main URL
+        request_url = self.main_url + '/groups'
+
+        # If workspace ID was not informed, return error message...
+        if workspace_id == '':
+            return {'message': 'Missing workspace id, please check.', 'content': ''}
+
+        # If workspace ID was informed...
+        else:
+            request_url = f'{request_url}/{workspace_id}/dataflows/{dataflow_id}'
+
+            # Make the request
+            r = requests.get(url=request_url, headers=self.headers)
+
+            # Get HTTP status and content
+            status = r.status_code
+            response = json.loads(r.content)
+            response['pbi:mashup']['allowNativeQueries'] = False
+
+            # If success...
+            if status == 200:
+                # Save to json file
+                filename = f'{self.dataflows_dir}/prod backup/{dataflow_name}.json'
+
+                with open(filename, mode='w', encoding='utf-8-sig') as f:
+                    json.dump(response, f, ensure_ascii=True, indent=4)
+                
+                return {'message': 'Success', 'content': response}
+
+            else:                
+                # If any error happens, return message.
+                response = json.loads(r.content)
+                error_message = response['error']['message']
+
+                return {'message': {'error': error_message, 'content': response}}
