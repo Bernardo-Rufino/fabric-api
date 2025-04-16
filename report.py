8282cf21aa4d7f7da467df68562f366d090962af
Bernardo Rufino <b.rufino@live.com>
2025-04-16 08:18:25 -0300

    Added object for reports data and exporting.

---
+++ report.py
@@ -0,0 +1,120 @@
+import os
+import json
+import requests
+import pandas as pd
+import dataset
+from typing import Dict
+from utilities import create_directory
+
+
+class Report:
+
+    def __init__(self, token: str):
+        """
+        Initialize variables.
+        """
+        self.main_url = 'https://api.powerbi.com/v1.0/myorg'
+        self.token = token
+        self.headers = {'Authorization': f'Bearer {self.token}'}
+        self.data_dir = './data/reports'
+
+        create_directory(self.data_dir)
+
+
+    def list_reports(
+                self, 
+                workspace_id: str = '') -> Dict:
+        """
+        List all reports on a specific workspace_id that the user has access to.
+
+        Args:
+            workspace_id (str, optional): workspace id to search datasets from.
+
+        Returns:
+            Dict: status message and content.
+        """
+
+        # Main URL
+        request_url = f'{self.main_url}/groups/{workspace_id}/reports'
+
+        # If workspace ID was not informed, return error message...
+        if workspace_id == '':
+            return {'message': 'Missing workspace id, please check.', 'content': ''}
+
+        # If workspace ID was informed...
+        else: 
+            filename = f'reports_{workspace_id}.xlsx'
+
+            # Make the request
+            r = requests.get(url=request_url, headers=self.headers)
+
+            # Get HTTP status and content
+            status = r.status_code
+            response = json.loads(r.content).get('value', '')
+
+            # If success...
+            if status == 200:
+                # Save to Excel file
+                df = pd.DataFrame(response)
+                df.to_excel(f'{self.data_dir}/{filename}', index=False)
+                
+                return {'message': 'Success', 'content': response}
+
+            else:                
+                # If any error happens, return message.
+                response = json.loads(r.content)
+                error_message = response['error']['message']
+
+                return {'message': {'error': error_message, 'content': response}}
+
+
+    def export_report(
+                self, 
+                workspace_id: str = '',
+                report_id: str = '',
+                report_name: str = '') -> Dict:
+        """
+        Export a specific report to a .pbix file.
+
+        Args:
+            workspace_id (str, optional): workspace id to search datasets from.
+            report_id (str, optional): report id to be exported.
+            report_name (str, optional): report name to be saved.
+
+        Returns:
+            Dict: status message and content.
+        """
+
+        # Main URL
+        request_url = f'{self.main_url}/groups/{workspace_id}/reports/{report_id}/Export/?DownloadType=LiveConnect'
+
+        # If workspace ID was not informed, return error message...
+        if workspace_id == '':
+            return {'message': 'Missing workspace id, please check.', 'content': ''}
+        if report_id == '':
+            return {'message': 'Missing report id, please check.', 'content': ''}
+
+        # If workspace ID and report ID were informed...
+        else: 
+            filename = f'{report_name}.pbix'
+            file_path = f'{self.data_dir}/exports'
+
+            create_directory(file_path)
+
+            # Make the request
+            r = requests.get(url=request_url, headers=self.headers)
+
+            # Get HTTP status and content
+            status = r.status_code
+
+            # If success...
+            if status == 200:
+                # Save to Excel file
+                with open(f'{file_path}/{filename}', 'wb') as f:
+                    f.write(r.content)
+                
+                return {'message': 'Success', 'content': 'File downloaded successfully.'}
+
+            else:                
+
+                return {'message': {'error': f'Error with status code {status}', 'content': ''}}
